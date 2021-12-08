4d6c88015d29ed01d355b7689eb23bf8951cc2ec
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2021-12-08 22:32:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var { color } = require('./lib/color.js')
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api'),
-    apiirouter = require('./views/api2')
+  
     
 var app = express()
 app.enable('trust proxy');
@@ -17,7 +17,7 @@ app.use(express.static("public"))
 
 app.use('/', mainrouter)
 app.use('/api', apirouter)
-app.use('/api2', apiirouter)
+
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
